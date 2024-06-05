54b16189ddb07f5c37c193e4e7ef19d6e8f81643
Tejiri Adebawojo <tejirioghenekaro@gmail.com>
2024-06-05 10:44:41 +0100

    Add Primitive Data

---
+++ Week3/app.js
@@ -0,0 +1,80 @@
+console.log("Hello world!");
+let name = "Tejiri";
+console.log(name);
+
+let age = "30";
+console.log(age);
+
+let myColor = "blue";
+console.log(myColor);
+
+let myFavouriteFood = "rice";
+myFavouriteFood = "beans";
+console.log(myFavouriteFood);
+
+
+
+let myName = "Ada";
+console.log(myName);
+
+console.log(myName + " " + myFavouriteFood + 1);
+
+console.log(`${myName} ${myFavouriteFood} "1" `);
+
+console.log(`${myName} ${myFavouriteFood}` + " asd");
+
+let myOperation = (1 + 2) * 4;
+console.log(myOperation);
+
+let number = (1 + 2 + 3 + 5 + 7);
+console.log(number);
+
+let myFirstNumber = 1;
+let mySecondNumber = 2;
+myFirstNumber = mySecondNumber 
+mySecondNumber = myFirstNumber
+console.log(myFirstNumber);
+console.log(mySecondNumber);
+
+
+let a = 1;
+let b = 2;
+
+a = b;
+b = a
+console.log(b);
+console.log(a);
+
+
+
+
+
+let myVariable = "variable"; //string
+let mySecondVariable = 5; //integer or number
+let myThirdVariable = false; //boolean
+
+console.log(myVariable + mySecondVariable + myThirdVariable);
+console.log(`${myVariable} ${mySecondVariable} ${myThirdVariable}`);
+
+let mySamNumber = 5;
+let mySecondSamNumber = 7;
+
+console.log(mySamNumber % mySecondSamNumber);
+
+
+let myComparison = mySamNumber > mySecondSamNumber; //greater than
+let myNextComparison = mySamNumber < mySecondSamNumber; //less than
+let myFollowingComparison = mySamNumber == mySecondSamNumber; // equal to
+console.log(myComparison);
+console.log(myNextComparison);
+console.log(myFollowingComparison);
+
+let firstNames = "Imhotep";
+console.log(`${firstNames} is ${firstNames.length} characters long`);
+
+let favouriteProgrammingLanguage = "JavaScript";
+let currentCourseDay = 3;
+
+const sentence = `My favourite programming language is ${favouriteProgrammingLanguage}. We're on day ${currentCourseDay} of the course.`;
+console.log(`My favourite programming language is ${favouriteProgrammingLanguage}. We're on day ${currentCourseDay} of the course.`);
+
