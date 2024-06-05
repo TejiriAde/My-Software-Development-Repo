5b1ef1b6e5b662505261ca33e187c6224d249d56
Tejiri Adebawojo <tejirioghenekaro@gmail.com>
2024-06-05 13:54:49 +0100

    Add Conditional

---
+++ Week/app.js
@@ -0,0 +1,12 @@
+
+
+let age = 10;
+let movieRating = 18;
+
+if (age > 18) {
+    console.log("You can watch any movie!");
+} else if (age === 18) {
+    console.log("You are just old enough to watch any movie!")
+} else {
+    console.log("You are not old enough to watch this movie");
+}
