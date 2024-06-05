7ac83d11190be5532d8dbd0dbe0e2ffe56749e43
Tejiri Adebawojo <tejirioghenekaro@gmail.com>
2024-06-05 13:13:35 +0100

    Add DOM

--- Week3/app.js
+++ Week3/app.js
@@ -1,80 +1,13 @@
-console.log("Hello world!");
-let name = "Tejiri";
-console.log(name);
 
-let age = "30";
-console.log(age);
+const h1 = document.querySelector("h1");
+h1.remove(h1);
+console.log(h1);
 
-let myColor = "blue";
-console.log(myColor);
+const body = document.querySelector("body");
+const h3 = document.createElement("h3");
+h3.textContent = "Testing DOM";
+body.appendChild(h3);
+console.log(h3);
 
-let myFavouriteFood = "rice";
-myFavouriteFood = "beans";
-console.log(myFavouriteFood);
-
-
-
-let myName = "Ada";
-console.log(myName);
-
-console.log(myName + " " + myFavouriteFood + 1);
-
-console.log(`${myName} ${myFavouriteFood} "1" `);
-
-console.log(`${myName} ${myFavouriteFood}` + " asd");
-
-let myOperation = (1 + 2) * 4;
-console.log(myOperation);
-
-let number = (1 + 2 + 3 + 5 + 7);
-console.log(number);
-
-let myFirstNumber = 1;
-let mySecondNumber = 2;
-myFirstNumber = mySecondNumber 
-mySecondNumber = myFirstNumber
-console.log(myFirstNumber);
-console.log(mySecondNumber);
-
-
-let a = 1;
-let b = 2;
-
-a = b;
-b = a
-console.log(b);
-console.log(a);
-
-
-
-
-
-let myVariable = "variable"; //string
-let mySecondVariable = 5; //integer or number
-let myThirdVariable = false; //boolean
-
-console.log(myVariable + mySecondVariable + myThirdVariable);
-console.log(`${myVariable} ${mySecondVariable} ${myThirdVariable}`);
-
-let mySamNumber = 5;
-let mySecondSamNumber = 7;
-
-console.log(mySamNumber % mySecondSamNumber);
-
-
-let myComparison = mySamNumber > mySecondSamNumber; //greater than
-let myNextComparison = mySamNumber < mySecondSamNumber; //less than
-let myFollowingComparison = mySamNumber == mySecondSamNumber; // equal to
-console.log(myComparison);
-console.log(myNextComparison);
-console.log(myFollowingComparison);
-
-let firstNames = "Imhotep";
-console.log(`${firstNames} is ${firstNames.length} characters long`);
-
-let favouriteProgrammingLanguage = "JavaScript";
-let currentCourseDay = 3;
-
-const sentence = `My favourite programming language is ${favouriteProgrammingLanguage}. We're on day ${currentCourseDay} of the course.`;
-console.log(`My favourite programming language is ${favouriteProgrammingLanguage}. We're on day ${currentCourseDay} of the course.`);
-
+const h4 = document.querySelector("h4");
+h4.textContent = "DOMs";
